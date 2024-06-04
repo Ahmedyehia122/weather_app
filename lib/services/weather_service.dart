fbd5c3f9cef494e77edb8d5a3806b24e277a8b7c
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-06-04 14:21:57 +0300

    handling exeptions

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,10 +6,21 @@ class WeatherService {
   WeatherService(this.dio);
   final String baseUrl = 'https://api.weatherapi.com/v1';
   final String apiKey = '1be1e466dbec4c85966120552240306';
+  final String customErrMessage =
+      'oops there was an error, please try again later!';
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response =
-        await dio.get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errMessage =
+          e.response?.data['error']['message'] ?? customErrMessage;
+      throw Exception(errMessage);
+    } catch (e) {
+      throw Exception(customErrMessage);
+    }
   }
 }
